4c56dde5cb29771f9c58b5bde5f1ee68e50abfb9
Nano191225 <93137582+Nano191225@users.noreply.github.com>
2024-02-10 21:00:14 +0900

    workflow10, i dont wanna use js if possible

---
+++ .github/workflows/rename_aseet.js
@@ -0,0 +1,24 @@
+// rename_asset.js
+const fs = require("fs");
+const axios = require("axios");
+const path = require("path");
+
+async function main() {
+    const token = process.env.GITHUB_TOKEN;
+    const repo = process.env.GITHUB_REPOSITORY;
+    const tag = process.env.GITHUB_REF.split("/").pop();
+    const releaseId = process.env.GITHUB_EVENT_PATH;
+
+    const { data: assets } = await axios.get(`https://api.github.com/repos/${repo}/releases/${releaseId}/assets`, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    });
+
+    const oldName = assets[0].name;
+    const newName = `${repo}-${tag}${path.extname(oldName)}`;
+
+    fs.renameSync(oldName, newName);
+}
+
+main().catch((err) => console.error(err));
