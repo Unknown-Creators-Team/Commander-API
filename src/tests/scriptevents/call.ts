19160f8e7e51e0517871cb28fa5fb48abccc9e53
arutaka1220 <97819325+arutaka1220@users.noreply.github.com>
2025-12-02 20:27:41 +0900

    refactor: update damage command in scriptevent_call test and improve event handling

--- src/tests/scriptevents/call.ts
+++ src/tests/scriptevents/call.ts
@@ -1,3 +1,4 @@
+import { EntityDamageCause, EntityHurtAfterEvent, world } from "@minecraft/server";
 import { ScoreboardDatabase } from "lib/DatabaseMC.js";
 import Test from "lib/Test.js";
 import call from "../../scriptevents/call.js";
@@ -5,22 +6,30 @@ import call from "../../scriptevents/call.js";
 new Test("scriptevent_call", "empty")
     .initialize((player) => { })
     .run(async (player) => {
+        // playerに対してcallで設定したダメージコマンドが正しく実行されるか確認する
         const testCallName = "for_test_" + Math.random().toString(36).substring(2);
         const database = new ScoreboardDatabase<string, string[]>(`CAPI_CALLS`);
-
-        // save test data
-        database.set(testCallName, ["say Hi"]);
+        database.set(testCallName, [`damage ${player.name} 1 piston`]);
 
         const callData = {
             name: testCallName,
             function: "test_function",
         };
 
+        async function event({ hurtEntity: evPlayer, damageSource: { cause } }: EntityHurtAfterEvent) {
+            if (player.id === evPlayer.id && cause === EntityDamageCause.piston) {
+                world.afterEvents.entityHurt.unsubscribe(event);
+            }
+        }
+
+        world.afterEvents.entityHurt.subscribe(event);
+
         try {
             call(player, JSON.stringify(callData));
         } catch (e) {
             throw e;
         } finally {
+            world.afterEvents.entityHurt.unsubscribe(event);
             database.delete(testCallName);
         }
     })
