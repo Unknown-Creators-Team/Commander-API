25ebae308223c2819e989b50a24e46a797f24f5f
Nano191225 <93137582+Nano191225@users.noreply.github.com>
2026-03-19 18:09:18 +0900

    update webhook.yml, manifest.json and src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,6 @@
  * @LINK https://github.com/191225/Commander-API
  */
 
-import { Timings } from "@bedrock-oss/bedrock-boost";
 import * as Minecraft from "@minecraft/server";
 import { CHANNEL, VERSION } from "constants.js";
 import { ScoreboardUtils } from "script-box-mc";
@@ -41,15 +40,16 @@ world.afterEvents.worldLoad.subscribe(async (ev) => {
             } else if (id === "capi:version" && sourceEntity?.isPlayer()) {
                 const channel = CHANNEL.charAt(0).toUpperCase() + CHANNEL.slice(1);
                 sourceEntity.sendMessage(`Commander API v${VERSION} (Official ${channel} Build)`);
-            } else if (id === "capi:start") {
-                for (let i = 0; i < 10000; i++) {
-                    event.sourceEntity?.addTag(`tag${i}`);
-                }
-            } else if (id === "capi:stop") {
-                for (let i = 0; i < 10000; i++) {
-                    event.sourceEntity?.removeTag(`tag${i}`);
-                }
             }
+            // else if (id === "capi:start") {
+            //     for (let i = 0; i < 10000; i++) {
+            //         event.sourceEntity?.addTag(`tag${i}`);
+            //     }
+            // } else if (id === "capi:stop") {
+            //     for (let i = 0; i < 10000; i++) {
+            //         event.sourceEntity?.removeTag(`tag${i}`);
+            //     }
+            // }
         },
         { namespaces: ["capi"] },
     );
@@ -58,20 +58,6 @@ world.afterEvents.worldLoad.subscribe(async (ev) => {
     system.runInterval(() => {
         ScoreboardUtils.setScore("watchdog", "capi:world", -1);
     }, 20);
-});
-
-let lastTime = Date.now();
-const deltas: number[] = [];
-system.runInterval(() => {
-    const delta = Date.now() - lastTime;
-    deltas.push(delta);
-    if (deltas.length > 20 * 60) deltas.shift();
-    const average = deltas.reduce((a, b) => a + b, 0) / deltas.length;
-    const tps = Math.round(1000 / average * 100) / 100;
-    for (const player of world.getPlayers()) {
-        player.onScreenDisplay.setActionBar(`§lTPS: §r§c${tps.toFixed(2)}`);
-    }
-    lastTime = Date.now();
 });
 
 declare module "@minecraft/server" {
