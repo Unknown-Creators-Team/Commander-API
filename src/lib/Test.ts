30e2a96c50b844c5c5a9a9de1564cc1dea286bfa
arutaka1220 <97819325+arutaka1220@users.noreply.github.com>
2026-07-21 15:47:50 +0900

    fix: convert gametest run coordinates to space-separated format

--- src/lib/Test.ts
+++ src/lib/Test.ts
@@ -81,7 +81,7 @@ export default class Test {
     public static runTest(name: string, location: Vec3): void {
         if (!this.tests.includes(name)) throw new Error(`Test "${name}" is not registered.`);
         // /execute positioned 0 0 0 run gametest run testName
-        world.getDimension("overworld").runCommand(`execute positioned ${location.add([0, 0, 1]).floor().toString()} run gametest run capi:${name}`);
+        world.getDimension("overworld").runCommand(`execute positioned ${location.add([0, 0, 1]).floor().toArray().join(" ")} run gametest run capi:${name}`);
     }
 
     public static runAll(location: Vec3): void {
